5aa39fa9a06c7ca8a8afe8bfa91bbcec3bf4ffd8
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-19 20:21:36 +0100

    ✨ Function to get fields in DESCRIPTION

--- R/desc.R
+++ R/desc.R
@@ -10,6 +10,7 @@ get_package_desc <- function(archive) {
 
     desc_file <- get_file_in_archive(archive, paste(package_name, "DESCRIPTION", sep = "/"))
     on.exit(file.remove(desc_file), add = TRUE)
+    # on.exit(unlink(dirname(desc_file)), add = TRUE)
 
     desc <- tryCatch(read.dcf(desc_file), error = identity)
 
@@ -17,4 +18,27 @@ get_package_desc <- function(archive) {
         stop("Malformed DESCRIPTION in ", archive)
 
     return(desc[1L, ])
+}
+
+
+#' Get fields from DESCRIPTION file
+#'
+#' @inheritParams get_file_in_archive
+#' @param field `[character]` A vector of field names to return.
+#'
+#' @return The fields in a named vector.
+#'
+#' @export
+get_desc_field <- function(archive, field) {
+    assertthat::assert_that(
+        is.character(field)
+    )
+
+    desc <- get_package_desc(archive)
+
+    desc_entry_matches <- field %in% names(desc)
+    if (!all(desc_entry_matches))
+        stop("These fields are not in DESCRIPTION: ", paste(field[!desc_entry_matches], collapse = ", "))
+
+    desc[field]
 }
