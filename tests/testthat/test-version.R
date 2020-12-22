2f757fcbadce1dc0f5b69a6390946752b7eb9973
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-22 08:57:21 +0100

    ✨ Get the version of a package

--- tests/testthat/test-version.R
+++ tests/testthat/test-version.R
@@ -3,3 +3,15 @@ test_that("Get R version from binary package", {
 
     expect_equal(r_version, getRversion())
 })
+
+
+test_that("Error getting R version from source package", {
+    expect_error(get_r_version(source_package), regexp = "is not a binary package")
+})
+
+
+test_that("Get package version from binary package", {
+    package_version <- get_package_version(source_package)
+
+    expect_equal(package_version, package_version("0.0.1"))
+})
