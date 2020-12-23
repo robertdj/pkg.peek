625d7298aa73bc8250f86dc483fad7b732790efa
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2020-12-23 11:53:34 +0100

    💥 Change order of arguments in low level function

--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -1,10 +1,10 @@
 test_that("Get NAMESPACE from source package", {
-    n <- get_file_in_archive(source_package, "foo/NAMESPACE")
+    n <- get_file_in_archive("foo/NAMESPACE", source_package)
 
     expect_true(startsWith(readLines(n), "exportPattern"))
 })
 
 
 test_that("Error getting non-existing file", {
-    expect_error(get_file_in_archive(source_package, "NAMESPACE"))
+    expect_error(get_file_in_archive("NAMESPACE", source_package))
 })
